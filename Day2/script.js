16819a049ea5c0806112ae421301bdb067d1584f
luis.r.perezibanez.mil@army.mil <luis.r.perezibanez.mil@army.mil>
2024-08-28 10:54:18 -0500

    Added Day2 Folder

---
+++ Day2/script.js
@@ -0,0 +1,17 @@
+
+let testArray = ["Hello", "World", "This", "Is", "A", "Test"];
+
+let value = testArray.unshift("New Element");
+
+console.log(testArray);
+
+let num = 0;
+
+testArray.forEach(function(){
+    console.log(num++);
+});
+
+let numberArray = [1, 5, 100, 3, 6, 200, 20]
+
+console.log(numberArray.sort());
+
