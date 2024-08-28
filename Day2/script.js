4b718b5258d06d96e1c6d602446a21df1cd13241
luis.r.perezibanez.mil@army.mil <luis.r.perezibanez.mil@army.mil>
2024-08-28 11:53:32 -0500

    added more samples

--- Day2/script.js
+++ Day2/script.js
@@ -11,7 +11,13 @@ testArray.forEach(function(){
     console.log(num++);
 });
 
-let numberArray = [1, 5, 100, 3, 6, 200, 20]
+let numberArray = [1, 5, 100, 3, 6, 200, 20, 1.1]
 
 console.log(numberArray.sort());
 
+console.log(numberArray.filter(function(number){
+    return number < 10
+}));
+
+
+console.log(numberArray.filter(num => num < 5));
