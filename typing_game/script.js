ec8172113a0f14ab62ad7acb3840b84071d4882e
luis.r.perezibanez.mil@army.mil <luis.r.perezibanez.mil@army.mil>
2024-09-12 12:13:34 -0500

    added words per minute selection and game setup feature

--- typing_game/script.js
+++ typing_game/script.js
@@ -2,6 +2,7 @@ const timerInterval = 10; // milliseconds
 let timerId = setInterval(engine, timerInterval);
 
 const typingTestParagraphs = [
+"Type the words as fast as you can. Do not let the container fill up. :)",
 "The quick brown fox jumps over the lazy dog. This classic sentence contains every letter of the English alphabet and is often used to demonstrate typewriters and computer keyboards. It is a great way to assess typing speed and accuracy. By practicing with this sentence, you can improve your typing skills and become more proficient at handling various types of text. Remember to keep your fingers on the home row and maintain a steady rhythm as you type. Consistent practice can lead to significant improvements over time.",
 "The gentle breeze rustles through the tall, verdant grass. This serene scene captures the essence of nature's tranquility and is often used to evoke a sense of peace and relaxation. Observing this natural beauty can be a wonderful way to relieve stress and connect with the environment. By immersing yourself in such moments, you can foster a deeper appreciation for the outdoors and enhance your overall well-being. Remember to take a deep breath and fully engage your senses as you enjoy the surroundings. Consistent moments of mindfulness in nature can lead to greater mental clarity and inner calm.",
 "Bright stars twinkle in the vast night sky. This captivating view highlights the beauty of the cosmos and is often used to inspire wonder and curiosity about the universe. Stargazing is a wonderful way to gain perspective on our place in the world and deepen your understanding of astronomy. By observing celestial events and constellations, you can expand your knowledge of the night sky and develop a greater appreciation for its mysteries. Remember to use a telescope for a closer look and keep a stargazing journal to track your discoveries. Regularly exploring the stars can lead to fascinating insights and a profound sense of connection to the universe.",
@@ -9,12 +10,12 @@ const typingTestParagraphs = [
 ];
 
 let selectedParagraph = 0;
-let WORDS = [];
+let WORDS = typingTestParagraphs[selectedParagraph].split(" ");
 
 let PARAGRAPH_COUNTER = 0;
 let HELPER_COUNTER = 0;
 let VISIBLE_SPEED = 0; 
-let SELECTED_SPEED = 150; // How many times 'timeInterval' has to pass to sho a new game button. 100 = every second.
+let SELECTED_SPEED = 100; // How many times 'timeInterval' has to pass to sho a new game button. 100 = every second.
 
 let helperIsSelected = true;
 let helperHighlited = false;
@@ -23,14 +24,42 @@ const numberOfColumns = 4;
 const numberOfRows = 4;
 let visibleCount = 0;
 
+function setup() {
+    let setupCounter = 0;
+    for(let i = 0; i < numberOfColumns; i++){
+        for(let j = 0; j < numberOfRows; j++){
+            let btnId = "btn" + i + "-" + j;
+            let tempBtn = document.getElementById(btnId);
+
+            let word = getWordAtIndex(typingTestParagraphs[selectedParagraph], setupCounter++)
+
+            // Create a span for each letter and add it to the container
+            word.split('').forEach(letter => {
+                const span = document.createElement('span');
+                span.textContent = letter;
+                span.classList.add('letter');
+                tempBtn.appendChild(span);
+            });
+
+
+            tempBtn.setAttribute("value", word);
+            
+        }
+    }
+}
+
 function initializeGame(){
     stopGame();
+
+    SELECTED_SPEED = document.getElementById("speedSelector").value * 1;
 
     PARAGRAPH_COUNTER = 0;
     HELPER_COUNTER = 0;
     VISIBLE_SPEED = 10;
 
-    selectedParagraph = 3//getRandomIntegerInclusive(typingTestParagraphs.length - 1);
+    selectedParagraph = getRandomIntegerInclusive(typingTestParagraphs.length - 1);
+    if(selectedParagraph == 0)
+        selectedParagraph++;
 
     WORDS = typingTestParagraphs[selectedParagraph].split(" ");
 
@@ -39,7 +68,6 @@ function initializeGame(){
 
     visibleCount = 0;
 
-    let numberIndex = 0;
     for(let i = 0; i < numberOfColumns; i++){
         for(let j = 0; j < numberOfRows; j++){
             let btnId = "btn" + i + "-" + j;
@@ -50,20 +78,9 @@ function initializeGame(){
             tempBtn.setAttribute("isBlocked", "false");
             tempBtn.classList.remove("gameBtnHighlight");
             tempBtn.style.opacity = 0;
-
-            numberIndex++;
         }
     }
     startGame();
-}
-
-// Function to shuffle an array using the Fisher-Yates algorithm --> Pulled from the internets.
-function shuffleArray(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]]; // Swap elements
-    }
-    return array;
 }
 
 function engine() {
@@ -98,8 +115,7 @@ function engine() {
 
         if(tempBtn.getAttribute("isVisible") == "true"){
             stopGame();
-            alert("You have lost. Press OK to restart");
-            initializeGame();
+            alert("You have lost.");
         }
         else{
 
@@ -178,48 +194,49 @@ function getWordAtIndex(paragraph, index) {
 
 function handleKeyDown(event) {
     // `event` contains information about the keyboard event
-    console.log('Key pressed:', event.key); // Logs the key pressed
+    console.log('Key pressed:', event.keyCode); // Logs the key pressed
 
-    let btnId = "";
+    if(event.keyCode != 32){
+        let btnId = "";
 
-    for(let i = 0; i < 4; i++){
-        for(let j = 0; j < 4; j++){
-            btnId = "btn" + i + "-" + j;
-            let querySelector = "#"+btnId + " .letter";
+        for(let i = 0; i < 4; i++){
+            for(let j = 0; j < 4; j++){
+                btnId = "btn" + i + "-" + j;
+                let querySelector = "#"+btnId + " .letter";
 
-            const letter = event.key.toLowerCase(); // Convert to lowercase for comparison
-            const letters = document.querySelectorAll(querySelector);
+                const letter = event.key.toLowerCase(); // Convert to lowercase for comparison
+                const letters = document.querySelectorAll(querySelector);
 
-            // Loop through each letter and highlight if it matches the pressed key
-            for(let i = 0; i < letters.length; i++){
-                if (letters[i].textContent.toLowerCase() === letter && i == 0){
-                    letters[i].classList.add('highlight');
+                // Loop through each letter and highlight if it matches the pressed key
+                for(let i = 0; i < letters.length; i++){
+                    if (letters[i].textContent.toLowerCase() === letter && i == 0){
+                        letters[i].classList.add('highlight');
+                    }
+                    else if (i > 0 && letters[i].textContent.toLowerCase() === letter && letters[i-1].classList.contains('highlight'))
+                        letters[i].classList.add('highlight');
                 }
-                else if (i > 0 && letters[i].textContent.toLowerCase() === letter && letters[i-1].classList.contains('highlight'))
-                    letters[i].classList.add('highlight');
-            }
 
-            const allHighlighted = Array.from(letters).every(span => span.classList.contains('highlight'));
-            console.log(allHighlighted);
+                const allHighlighted = Array.from(letters).every(span => span.classList.contains('highlight'));
+                console.log(allHighlighted);
 
-            if (letters.length > 0 && allHighlighted) {
-                console.log('All letters have been highlighted.');
+                if (letters.length > 0 && allHighlighted) {
+                    console.log('All letters have been highlighted.');
 
-                let tempBtn = document.getElementById(btnId);
-                tempBtn.innerHTML = ''; // Clear any existing content
-                tempBtn.setAttribute("isVisible", "false");
-                tempBtn.setAttribute("value", "");
-                tempBtn.style.opacity = 0;
-                visibleCount--;
-                if(tempBtn.classList.contains("gameBtnHighlight")){
-                    tempBtn.classList.remove("gameBtnHighlight");
-                    helperHighlited = false;
+                    let tempBtn = document.getElementById(btnId);
+                    tempBtn.innerHTML = ''; // Clear any existing content
+                    tempBtn.setAttribute("isVisible", "false");
+                    tempBtn.setAttribute("value", "");
+                    tempBtn.style.opacity = 0;
+                    visibleCount--;
+                    if(tempBtn.classList.contains("gameBtnHighlight")){
+                        tempBtn.classList.remove("gameBtnHighlight");
+                        helperHighlited = false;
+                    }
+                
                 }
-            
             }
         }
     }
-
 }
 
 function getRandomIntegerInclusive(max) {
@@ -229,5 +246,6 @@ function getRandomIntegerInclusive(max) {
 // Add event listener for 'keydown' events on the document
 document.addEventListener('keydown', handleKeyDown);
 
+setup();
 stopGame();
 //initializeGame();
